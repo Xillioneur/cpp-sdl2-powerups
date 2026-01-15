3c32a1542d6bcbecc8e1f59b5896c2b19609a571
Xillioneur <241650870+Xillioneur@users.noreply.github.com>
2026-01-15 06:33:47 -0500

    Create the project folder and GitHub repo

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#define _USE_MATH_DEFINES
+#include <SDL2/SDL.h>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstdbool>
+#include <string>
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
